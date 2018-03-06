80f9c23e0d48bcfacfd3fd0a9c8196494ea004e1
Scott Johnson <fuzzygroup@gmail.com>
2018-03-06 16:00:36 -0500

    Rework of the core vending_machine class - sell, list_inventory, initialize and more

--- vending_machine.rb
+++ vending_machine.rb
@@ -2,17 +2,85 @@ Dir["lib/**.*"].each {|file| require_relative file}
 
 
 class VendingMachine
-  ACTIONS = [:buy, :pay, :refund, :cancel, :look, :status, :inventory, :stock, :restock]
+  ACTIONS = [:buy, :pay, :refund, :cancel, :look, :status, :inventory, :stock, :restock, :quit]
+  
+  attr_accessor :products
   
   def initialize
-    @customer = Customer.new    
+    #
+    # Create a customer 
+    #
+    @customer = Customer.new
+    
+    # 
+    # Create a hash of products
+    #
+    @products = {
+      "ABC" => Product.new("Snickers", 1.25),
+      "DEF" => Product.new("Granola Bar", 0.75),
+      "GHI" => Product.new("Doritos", 1.00),
+      "JKL" => Product.new("Potato Chips", 1.00),
+      "MNO" => Product.new("Twinkies", 0.95),
+      "PQR" => Product.new("Diet Coke", 1.00),
+    }
+    
+    #
+    # Start the actual vending machine
+    #
     start_machine
   end
+
+  #
+  # Return back an array of product_codes that can be used to check for inclusion
+  #
+  def product_codes
+    codes = []
+    @products.each do |product_code, product|
+      codes << k
+    end
+    codes
+  end
   
-  private
+  #
+  # Check if we are sold out of a given product
+  #
+  def product_is_available?(product_code)
+    return true if @products[product_code].quantity > 0
+    return false
+  end
   
+  def sell(product_code)
+    selected_product = @products[product_code]
+    
+    if product_is_available?(product_code) && @customer.wallet > selected_product.price
+      puts "Success!  You have purchased the desired product"
+      
+      # Decrement the product to indicate we sold something
+      selected_product.decrease_quantity
+      
+      # Decrement the wallet
+      @customer.wallet = @customer.wallet - selected_product.price
+    
+      
+      # Restock the product; Assume a just in time inventory approach so we re-order every time we sell something
+      selected_product.restock
+      
+    else
+    end
+  end
+    
   def list_inventory
-    Inventory.list.join(", ")
+    puts "\n"
+    self.products.each do |product_code,product|
+      puts "#{product_code}: #{product.name} - $#{product.price}"
+    end
+    puts "\n"
+    nil
+  end
+  
+  def list_commands
+    commands = ACTIONS.map(&:to_s)
+    puts "Commands: #{commands.join(' ')}"
   end
   
   def start_machine
@@ -25,7 +93,7 @@ class VendingMachine
   end
 
   def take_customer_input
-    print "What do you want to buy?"
+    print "What do you want to do?"
     gets.chomp.to_sym
   end
   
@@ -35,7 +103,11 @@ class VendingMachine
   
   def print_status
     puts "As a weary adventurer, you find yourself in a #{get_weather} forest.  In front of you stands a gleaming vending machine filled with tasty treats."
-    puts "Things you can buy include: #{list_inventory}"
+    puts "Things you can buy include:"
+    list_inventory
+    list_commands
+    puts "You have: $#{@customer.list_money} to spend."
+    @customer.list_money
   end
   
   def take_action(action)
@@ -56,7 +128,14 @@ class VendingMachine
       
     when :restock
       
+    when :quit
+      quit
     end
+  end
+  
+  def quit
+    puts "\n\nThanks for trying this out; I can be reached at fuzzygroup@gmail.com or 317 531 4853 if you want to take the next step"
+    exit
   end
   
 end
