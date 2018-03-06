ff647dedf4618d383146ce2c55e14cdf69c0e247
Scott Johnson <fuzzygroup@gmail.com>
2018-03-06 15:57:51 -0500

    Add methods for initialize, buy? and list_money

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,17 @@
 class Customer
+  
+  attr_accessor :wallet
+  
+  def initialize
+    self.wallet = 10.00
+  end
+  
+  def buy?(amount)
+    return true if self.wallet > amount
+    return false
+  end
+  
+  def list_money
+    return self.wallet
+  end
 end
